4978b0622604521c7726085d2b9f0635af574321
Travis Thompson <Travo100@gmail.com>
2014-07-04 23:13:01 -0700

    Added admin panel for social media and contact info

---
+++ panel_script.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($){
+  $('.all_options:not(:eq(0))').slideUp();
+  $('.input_title').click(function(){
+    if($(this).next('.all_options').css('display')=='none') {
+      $(this)
+        .removeClass('inactive').addClass('active')
+        .children('img')
+          .removeClass('inactive').addClass('active');
+    } else {
+      $(this)
+        .removeClass('active').addClass('inactive')
+        .children('img')
+          .removeClass('active').addClass('inactive');
+    }
+    $(this).next('.all_options').slideToggle('slow');
+  });
+});
